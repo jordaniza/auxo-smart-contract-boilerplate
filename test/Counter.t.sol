20a3d80da3ea3308dfd6ca87ea6fb575bcf2fd67
jordan <jordaniza@proton.me>
2022-10-19 11:55:37 +0400

    starter with readme and slither

--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -1,14 +1,14 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "forge-std/Test.sol";
+import "@forge-std/Test.sol";
 import "../src/Counter.sol";
 
 contract CounterTest is Test {
     Counter public counter;
 
     function setUp() public {
-        counter = new Counter();
+        counter = new Counter(address(0));
         counter.setNumber(0);
     }
 
